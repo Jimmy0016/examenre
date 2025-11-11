661f1250e18d628016bea8c791f24ed9459bf667
JordySZ <jam.sanchez@yavirac.edu.ec>
2025-11-11 15:21:56 -0500

    Convertir 2

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hola desde CI/CD con GitHub Actions!")
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"mensaje": "👋 Hola Mundo desde Flask y CI/CD con GitHub Actions 🚀"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- test_app.py
+++ test_app.py
@@ -2,6 +2,6 @@ from app import app
 
 def test_home():
     client = app.test_client()
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
-    assert b"Hola Mundo" in response.data
+    assert b"Hola Mundo!" in response.data

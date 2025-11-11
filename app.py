61e7f2bd12f6b144d753f7d77a878a47715d45c5
JordySZ <jam.sanchez@yavirac.edu.ec>
2025-11-11 15:41:25 -0500

    Corregir etiqueta Docker a minúsculas

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
-
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello():
     return "Hola Mundo"
+
+if __name__ == "__main__":
+    # Escucha en todas las interfaces (necesario para Docker)
+    app.run(host="0.0.0.0", port=5000)

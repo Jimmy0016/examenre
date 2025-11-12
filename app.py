c003ce6dba040cffddb503f71d30cc5952721290
JordySZ <jam.sanchez@yavirac.edu.ec>
2025-11-12 16:47:12 -0500

    Agregado2

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hola Mundo"
+
 
 if __name__ == "__main__":
     # Escucha en todas las interfaces (necesario para Docker)

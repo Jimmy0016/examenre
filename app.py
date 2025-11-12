e3eefc8a583028003930929bc2625e75596bac08
JordySZ <jam.sanchez@yavirac.edu.ec>
2025-11-12 16:42:13 -0500

    Agregado

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def hello():
 
 if __name__ == "__main__":
     # Escucha en todas las interfaces (necesario para Docker)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)

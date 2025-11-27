3c6ad8945f382cc843ce6e80ccc42415d66df54a
Jimmy0016 <jrn.espinoza@yavirac.edu.ec>
2025-11-27 18:43:24 -0500

    first

--- tests/test_app.py
+++ tests/test_app.py
@@ -17,4 +17,4 @@ def test_home_content(client):
     """Verifica que aparece el texto correcto"""
     response = client.get('/')
     # Buscamos la palabra clave en el HTML devuelto
-    assert b"Hola Mundo" in response.data
+    assert b"JIMMY ESPINOZA SILENCE" in response.data
